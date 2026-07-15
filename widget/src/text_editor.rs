6894273c4e2bda58b6e2e7e50ff710d0d1f24938
dtzxporter <dtzxporter@users.noreply.github.com>
2026-07-15 11:35:30 -0400

    Fix text_editor selection rendering.

--- widget/src/text_editor.rs
+++ widget/src/text_editor.rs
@@ -984,7 +984,7 @@ where
                     {
                         renderer.fill_quad(
                             renderer::Quad {
-                                bounds: range,
+                                bounds: range.round(),
                                 ..renderer::Quad::default()
                             },
                             style.selection,
